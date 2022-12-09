b41c4cf88a7a990e8dcc6002cec3c458095d9430
Siva-Komma <kommasivajyothi22@gmail.com>
2022-12-09 20:35:58 +0530

    [Siva-Komma]Variables

---
+++ Variables/LocalVariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class LocalVariables
+    {
+        public void Local()
+        {
+            int var1 = 0;
+            int var2 = 0;
+            int sum;
+            sum = var1 + var2;
+            Console.WriteLine("Sum of the numbers:" +sum);
+            Console.ReadLine();
+            
+        }
+
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            LocalVariables lc = new LocalVariables();
+            Console.WriteLine("Enetr Var1 ");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Var2 ");
+            int var2 = Convert.ToInt32(Console.ReadLine());
+            lc.Local();
+            Console.ReadLine();
+
+        }
+    }
+}
